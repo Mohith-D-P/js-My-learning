2877a6bab2107af06f1c155d8b86167621ae026b
MOHITH D P <119958455+Mohith-D-P@users.noreply.github.com>
2023-08-07 22:31:25 +0530

    Basics -js

---
+++ first.js
@@ -0,0 +1,47 @@
+// var a =10+"20";
+// console.log(a);
+
+// console.log(typeof(a));
+
+//2 bug 
+// var a =9-"5";
+// console.log(a); it is a bug
+
+// console.log(typeof(a));
+
+//3 bug
+
+// var a ="java"+"script";
+// console.log(a);
+
+// console.log(typeof(a));
+
+// var a =" "+" ";
+// console.log(a);
+
+// console.log(typeof(a));
+
+// var a ="vinod"+"Thaapa";
+// console.log(a);
+
+// console.log(typeof(a));
+
+// var a =true+true;
+// console.log(a);
+
+// console.log(typeof(a));
+
+// console.log(NaN===NaN);not a number=true
+
+// console.log(Number.NaN===NaN);
+
+//control constructs
+
+var tomm='sunny';
+if(tomm=='rain')
+{
+console.log('take rain coat');
+}
+else{
+    console.log('raincoat not necessary');
+}
